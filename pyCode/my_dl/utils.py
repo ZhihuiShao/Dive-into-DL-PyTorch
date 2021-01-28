22049c0a1bac7c7cb508b93f1f53df456a8212e6
ZhihuiShao <shaozhihui15@163.com>
2021-01-28 14:32:47 -0800

    f

--- pyCode/my_dl/utils.py
+++ pyCode/my_dl/utils.py
@@ -24,8 +24,158 @@ import numpy as np
 
 
 def load_data_fashion_mnist(batch_size,resize=None,root='~/Datasets/FashionMNIST'):
-    "Download fashion minist dataset and generate data loader in pytorch"
+    "Download fashion mnist dataset and generate data loader in pytorch"
     trans = [] #the transforms when loading data
     if resize: # resize is required
         trans.append(torchvision.transforms.Resize(size=resize))
-    trans.append
+    trans.append(torchvision.transforms.ToTensor())
+    transform = torchvision.transforms.Compose(trans)
+    # create dataset
+    mnist_train = torchvision.datasets.FashionMNIST(root=root,train=True,download=True,transform=transform)
+    mnist_test = torchvision.datasets.FashionMNIST(root=root,train=False,download=True,transform=transform)
+    # number of works
+    num_workers = 0 if sys.platform.startswith('win') else 4
+    # iteration (dataloader) for train and test
+    train_iter = torch.utils.data.DataLoader(mnist_train,batch_size=batch_size,shuffle=True,num_workers=num_workers)
+    test_iter = torch.utils.data.DataLoader(mnist_test,batch_size=batch_size,shuffle=False,num_workers=num_workers)
+    return train_iter,test_iter
+
+def train_ch5(net,train_iter,test_iter,batch_size,optimizer,device,num_epochs):
+    """
+    train network with train and test data, here the loss is crossentropy loss
+    """
+    net = net.to(device)
+    print('training on ',device)
+    loss = torch.nn.CrossEntropyLoss()
+    for epoch in range(num_epochs):
+        train_l_sum, train_acc_sum, n, batch_count, start = 0.0, 0.0, 0, 0, time.time()
+        for X, y in train_iter:
+            X = X.to(device)
+            y = y.to(device)
+            y_hat = net(X)
+            l = loss(y_hat,y)
+            optimizer.zero_grad()
+            l.backward()
+            optimizer.step()
+            train_l_sum += l.cpu().item()
+            train_acc_sum += (y_hat.argmax(dim=1)==y).sum().cpu().item()
+            n += y.shape[0]
+            batch_count += 1
+        train_acc = train_acc_sum/n
+        test_acc = evaluate_accuracy(test_iter, net,device=device)
+        print('epoch %d, loss %.4f, train acc %.3f, test acc %.3f, time %.1f sec' % (epoch+1,train_l_sum,train_acc,test_acc,time.time()-start))
+
+def evaluate_accuracy(data_iter, net, device='cpu'):
+    net = net.to(device)
+    acc_sum, n = 0.0, 0
+    with torch.no_grad():
+        for X, y in data_iter:
+            X = X.to(device)
+            y = y.to(device)
+            y_hat = net(X)
+            acc_sum += (y_hat.argmax(dim=1)==y).sum().cpu().item()
+            n += y.shape[0]
+    return acc_sum / n
+
+
+# residual network
+# residual bloxk
+class Residual(nn.Module):
+    def __init__(self,in_channels,out_channels,use_1x1conv=False,stride=1):
+        # here in_channels==out_channels is we not use use_1x1conv
+        super(Residual,self).__init__()
+        self.conv1 = nn.Conv2d(in_channels,out_channels,kernel_size=3,padding=1,stride=stride)
+        self.conv2 = nn.Conv2d(out_channels,out_channels,kernel_size=3,padding=1)
+        if use_1x1conv:
+            self.conv3 = nn.Conv2d(in_channels,out_channels,kernel_size=1,stride=stride)
+        else:
+            self.conv3 = None
+        self.bn1 = nn.BatchNorm2d(out_channels)
+        self.bn2 = nn.BatchNorm2d(out_channels)
+
+
+    def forward(self, X):
+        Y = F.relu(self.bn1(self.conv1(X)))
+        Y = self.bn2(self.conv2(Y))
+        if self.conv3:
+            X = self.conv3(X)
+        return F.relu(Y+X)
+
+def resnet_block(in_channels, out_channels, num_residuals, first_block=False):
+    if first_block:
+        assert in_channels == out_channels # 第一个模块的通道数同输入通道数一致
+    blk = []
+    for i in range(num_residuals):
+        if i == 0 and not first_block:
+            blk.append(Residual(in_channels, out_channels, use_1x1conv=True, stride=2))
+        else:
+            blk.append(Residual(out_channels, out_channels))
+    return nn.Sequential(*blk)
+
+# global pooling
+class GlobalAvgPool2d(nn.Module):
+    def __init__(self):
+        super(GlobalAvgPool2d,self).__init__()
+    
+    def forward(self,X):
+        return F.avg_pool2d(X,kernel_size=X.size()[-2:]) #stride is same as kernel_size
+
+# flatten
+class FlattenLayer(nn.Module):
+    def __init__(self):
+        super(FlattenLayer,self).__init__()
+    
+    def forward(self,X):
+        return X.view(X.shape[0],-1)
+
+
+# get resnet model
+def get_resnet():
+    # resudidula model
+    net = nn.Sequential(
+            nn.Conv2d(1, 64, kernel_size=7, stride=2, padding=3),
+            nn.BatchNorm2d(64), 
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=3, stride=2, padding=1))
+    net.add_module("resnet_block1", resnet_block(64, 64, 2, first_block=True))
+    net.add_module("resnet_block2", resnet_block(64, 128, 2))
+    net.add_module("resnet_block3", resnet_block(128, 256, 2))
+    net.add_module("resnet_block4", resnet_block(256, 512, 2))
+    net.add_module("global_avg_pool", GlobalAvgPool2d()) # Global
+    net.add_module("fc", nn.Sequential(FlattenLayer(), nn.Linear(512, 10)))
+    return net
+
+class LeNet(nn.Module):
+    def __init__(self):
+        super(LeNet, self).__init__()
+        self.conv = nn.Sequential(
+            nn.Conv2d(1, 6, 5), # in_channels, out_channels, kernel_size
+            nn.Sigmoid(),
+            nn.MaxPool2d(2, 2), # kernel_size, stride
+            nn.Conv2d(6, 16, 5),
+            nn.Sigmoid(),
+            nn.MaxPool2d(2, 2)
+        )
+        self.fc = nn.Sequential(
+            nn.Linear(16*4*4, 120),
+            nn.Sigmoid(),
+            nn.Linear(120, 84),
+            nn.Sigmoid(),
+            nn.Linear(84, 10)
+        )
+
+    def forward(self, img):
+        feature = self.conv(img)
+        output = self.fc(feature.view(img.shape[0], -1))
+        return output
+
+if __name__=="__main__":
+    # uni
+    print(sys.platform) # 'win32'
+    num_workers = 0 if sys.platform.startswith('win') else 4
+    print(num_workers)
+    train_iter,test_iter = load_data_fashion_mnist(16)
+    for X,y in train_iter:
+        print(X.size(),y.size())
+        print(y.numpy())
+        break
